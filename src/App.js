a1c8ff5e4b7ce4ad32a5e646c4600501bb8fddef
obeldennis <dennis.obel@azubiafrica.org>
2020-08-28 11:53:17 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// state data for 3 counters
+const data = [
+  { id: 1, value: 0 },
+  { id: 2, value: 0 },
+  { id: 3, value: 0 },
+];
+
+// Counter Component
+class Counter extends Component {
+  render() {
+    const { value } = this.props;
+    return (
+      <div className="counter">
+        <b>{value}</b>
+        <div className="counter-controls">
+          <button className="button is-danger is-small">-</button>
+          <button className="button is-success is-small">+</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <div>
+        {data.map((counter) => (
+          <Counter key={counter.id} value={counter.value} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
